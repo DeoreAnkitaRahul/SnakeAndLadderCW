d4270a1e94397c4649aa1e42285368b479d1b516
“DeoreAnkitaRahul” <ankitapatil375@gmail.com>
2023-07-10 12:06:15 +0530

    [Ankita] Add : UC2-Rolls the Die to get No

--- SnakeNLadder/SnakeNLadder/Position.cs
+++ SnakeNLadder/SnakeNLadder/Position.cs
@@ -10,9 +10,11 @@ namespace SnakeNLadder
     {
         public void GameMethod()
         {
-            int position = 0;
-            Console.WriteLine("Welcome Player 1, Let's start the Game");
-            Console.WriteLine("Player 1 is at position : " + position);
+            Random random = new Random();
+            int dice = random.Next(1, 7);
+            Console.WriteLine($"The Player rolls a dice and gets: {dice}");
         }
-    }
+        
 }
+    }
+
