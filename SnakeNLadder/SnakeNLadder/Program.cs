a7cb857c2b86e54cce7bef22b724deabd0212e56
“DeoreAnkitaRahul” <ankitapatil375@gmail.com>
2023-07-10 11:57:54 +0530

    [Ankita] Add : UC1-Play with Single Player

---
+++ SnakeNLadder/SnakeNLadder/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace SnakeNLadder
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("Welcome to the Snake and Ladder Game");
+            Position newGame = new Position();
+            newGame.GameMethod();
+        }
+    }
+}
